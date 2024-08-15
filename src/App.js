f17eb1725b1a7244bec07c86cd30072f462dae36
unknown <shravanigulhane73@gmail.com>
2024-08-15 16:12:24 +0530

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
+import Footer from './template/footer/Footer';
 import Header from './template/header/Header';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/template/footer/Footer.js
@@ -0,0 +1,13 @@
+
+function Footer()
+{
+    return(
+        <div>
+            <h1>welcome to Footer</h1>
+        </div>
+
+    )
+
+    
+}
+export default Footer;
